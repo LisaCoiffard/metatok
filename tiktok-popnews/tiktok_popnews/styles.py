d8707f44a0fa4f5288ededd3d82dddffe1879a28
duytsev <duytsev@gmail.com>
2023-10-13 12:04:33 +0200

    add webapp

---
+++ tiktok-popnews/tiktok_popnews/styles.py
@@ -0,0 +1,18 @@
+bg_medium_color = "#d12bd1"
+
+border_color = "#fff3"
+accent_color = "#c626c6"
+
+input_style = dict(
+    bg=bg_medium_color,
+    border_color=border_color,
+    border_width="1px",
+    p="4",
+)
+
+video_style = dict(
+    width="500px",
+    height="auto",
+    border_radius="15px 15px",
+    border="5px solid #555",
+)
